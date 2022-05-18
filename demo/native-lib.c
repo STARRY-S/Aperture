fc6456786f35051500343e5e9cd3128e2263bedd
Starry Wang <starry.wang@suse.com>
2022-05-18 22:46:11 +0800

    Added interface for change material number

--- demo/native-lib.c
+++ demo/native-lib.c
@@ -96,4 +96,11 @@ Java_moe_starrys_game_1engine_GLES3JNILib_spotLightStatus(JNIEnv *env, jclass cl
         return 0;
 }
 
+JNIEXPORT jint JNICALL
+Java_moe_starrys_game_1engine_GLES3JNILib_changeTextureNum(JNIEnv *env, jclass clazz,
+                                                           jint tex_num) {
+        ap_render_set_material_num(tex_num);
+}
+
 #endif
+
